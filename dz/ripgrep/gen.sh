a48fa09d2e5fb0faa69043b5edfe0e68c3ad34d1
Paul Batchelor <thisispaulbatchelor@gmail.com>
2025-01-06 22:13:35 -0500

    ripgrep notes

--- dz/ripgrep/gen.sh
+++ dz/ripgrep/gen.sh
@@ -1,4 +1,7 @@
 > a.db
 dagzet files.dz | sqlite3 a.db
+./dzbrowse/logparse.py ../../logs/ripgrep.log | sqlite3 a.db
+dzbrowse/gentagnodes.py a.db
+dzbrowse/import_code.py ../../code/ripgrep/crates/core/main.rs codestudy/ripgrep/crates/core/main.rs | sqlite3 a.db
 
-python3 $HOME/proj/dzbrowse/generate.py a.db
+./dzbrowse/generate.py a.db
